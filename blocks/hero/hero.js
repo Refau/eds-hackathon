0d661292d5492595237ba5b804f5b65841076728
Rémi Faucheux <remifaucheux@mbp-de-remi-2.eur.adobe.com>
2025-01-08 15:59:12 +0100

    ds

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,7 @@
 import { readBlockConfig } from '../../scripts/aem.js';
 
 function renderPlaceholder(config, block) {
+  console.log(block);
   block.textContent = '';
   block.appendChild(document.createRange().createContextualFragment(`
     <div class="teaser">
@@ -70,7 +71,7 @@ function createCards(config) {
 export default async function decorate(block) {
   const config = readBlockConfig(block);
   console.log(config);
-  console.log(block);
+
   config['details-button'] = !!(config['details-button'] || config['details-button'] === 'true');
   config['cart-button'] = !!(config['cart-button'] || config['cart-button'] === 'true');
 
