aa1ede2d4aba07ee994b285bbc05330e1ffcdef0
Rémi Faucheux <remifaucheux@MBP-de-Remi-2-001.lan>
2025-01-09 15:09:22 +0100

    nc

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -3,4 +3,8 @@ import {readBlockConfig} from "../../scripts/aem.js";
 export default async function decorate(block) {
   const config = readBlockConfig(block);
   console.log(config);
+  console.log(config['quote']);
+  console.log(config.quote);
+
+
 }
