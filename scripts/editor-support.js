bb9869ec51e0bce8d030ecabbe62c8bbf9a67b8f
Refau <148989541+Refau@users.noreply.github.com>
2025-01-11 15:15:34 +0100

    Update editor-support.js

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -1,4 +1,3 @@
-
 import {
   decorateBlock,
   decorateBlocks,
@@ -6,47 +5,10 @@ import {
   decorateIcons,
   decorateSections,
   loadBlock,
-  loadBlocks,
+  loadSections,
 } from './aem.js';
 import { decorateRichtext } from './editor-support-rte.js';
 import { decorateMain } from './scripts.js';
-
-/**
- *
- * @param {HTMLElement} block
- * Use this function to trigger a mutation for the UI editor overlay when you
- * have a scrollable block
- */
-function createMutation(block) {
-  block.setAttribute('xwalk-scroll-mutation', 'true');
-  block.querySelector('.carousel-slides').onscrollend = () => {
-    block.removeAttribute('xwalk-scroll-mutation');
-  };
-}
-
-function getState(block) {
-  if (block.matches('.accordion')) {
-    return [...block.querySelectorAll('details[open]')]
-      .map((details) => details.dataset.aueResource);
-  }
-  if (block.matches('.carousel')) {
-    return block.dataset.activeSlide;
-  }
-  return null;
-}
-
-function setState(block, state) {
-  if (block.matches('.accordion')) {
-    block.querySelectorAll('details').forEach((details) => {
-      details.open = state.includes(details.dataset.aueResource);
-    });
-  }
-  if (block.matches('.carousel')) {
-    block.style.display = null;
-    createMutation(block);
-
-  }
-}
 
 async function applyChanges(event) {
   // redecorate default content and blocks on patches (in the properties rail)
@@ -71,19 +33,16 @@ async function applyChanges(event) {
       element.insertAdjacentElement('afterend', newMain);
       decorateMain(newMain);
       decorateRichtext(newMain);
-      await loadBlocks(newMain);
+      await loadSections(newMain);
       element.remove();
       newMain.style.display = null;
       // eslint-disable-next-line no-use-before-define
       attachEventListners(newMain);
       return true;
     }
-    if (element.matches('.fragment-wrapper')) {
-      return false;
-    }
+
     const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
     if (block) {
-      const state = getState(block);
       const blockResource = block.getAttribute('data-aue-resource');
       const newBlock = parsedUpdate.querySelector(`[data-aue-resource="${blockResource}"]`);
       if (newBlock) {
@@ -95,7 +54,6 @@ async function applyChanges(event) {
         decorateRichtext(newBlock);
         await loadBlock(newBlock);
         block.remove();
-        setState(newBlock, state);
         newBlock.style.display = null;
         return true;
       }
@@ -113,7 +71,7 @@ async function applyChanges(event) {
           decorateRichtext(newSection);
           decorateSections(parentElement);
           decorateBlocks(parentElement);
-          await loadBlocks(parentElement);
+          await loadSections(parentElement);
           element.remove();
           newSection.style.display = null;
         } else {
@@ -130,36 +88,6 @@ async function applyChanges(event) {
   return false;
 }
 
-function handleSelection(event) {
-  const { detail } = event;
-  const resource = detail?.resource;
-
-  if (resource) {
-    const element = document.querySelector(`[data-aue-resource="${resource}"]`);
-    const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
-
-    if (block && block.matches('.accordion')) {
-      // close all details
-      block.querySelectorAll('details').forEach((details) => {
-        details.open = false;
-      });
-      const details = element.matches('details') ? element : element.querySelector('details');
-      details.open = true;
-    }
-
-    if (block && block.matches('.carousel')) {
-      createMutation(block);
-    }
-    if (block && block.matches('.tabs')) {
-      const tabs = [...block.querySelectorAll('.tabs-panel > div')];
-      const index = tabs.findIndex((tab) => tab.dataset.aueResource === resource);
-      if (index !== -1) {
-        block.querySelectorAll('.tabs-list button')[index]?.click();
-      }
-    }
-  }
-}
-
 function attachEventListners(main) {
   [
     'aue:content-patch',
@@ -167,13 +95,12 @@ function attachEventListners(main) {
     'aue:content-add',
     'aue:content-move',
     'aue:content-remove',
+    'aue:content-copy',
   ].forEach((eventType) => main?.addEventListener(eventType, async (event) => {
     event.stopPropagation();
     const applied = await applyChanges(event);
     if (!applied) window.location.reload();
   }));
-
-  main?.addEventListener('aue:ui-select', handleSelection);
 }
 
 attachEventListners(document.querySelector('main'));
